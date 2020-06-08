60677155377f1aa89960ec0fe97c2948d5854d91
TheSoulsHunter <konrad.romanski4@gmail.com>
2020-06-08 22:00:26 +0200

    add files

---
+++ AISD_short.py
@@ -0,0 +1,7 @@
+def main():
+    print("Wybierz program: ", sep="", end="")
+    program = input()
+    
+    print("Wybierz sposób wprowadzania danych [tabela/lista] [t/l]: ", sep="", end="")
+    sposob = input()
+main()
