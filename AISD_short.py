c25bc76337d586417b56e1b2deb13f7052ba9c79
Radzisz1906 <krzysiu.piotr.r@gmail.com>
2020-06-09 18:18:28 +0200

    Update AISD_short.py

--- AISD_short.py
+++ AISD_short.py
@@ -1,4 +1,6 @@
 import graphs
+import qlos
+from qlos import quick
 from graphs import Graph_M
 
 
@@ -32,14 +34,26 @@ def grafy():
         Matrix.hamilton_finder_all()
 
 
+def qui():
+    print("Podaj liste (po spacji)")
+    tab = input().split()
+    tab = list(map(int, tab))
+    piw = int(input("Podaj indeks piwotu(tablica indeksowana od 1):   ")) - 1
+    tab = quick(tab, piw)
+    tab.quicksort(tab.A)
+
+
 def main():
-    print("1. Grafy. \n")
+    print("1. Grafy. 2.Quick\n")
     print("Wybierz program: ", sep="", end="")
     program = int(input())
     if program == 1:
         grafy()
+    if program == 2:
+        qui()
     """print(
         "Wybierz sposób wprowadzania danych [tabela/lista] [t/l]: ", sep="", end="")"""
-    #sposob=input()
+    # sposob=input()
+
 
 main()
